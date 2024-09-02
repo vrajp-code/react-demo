39c668d22204f37c19a045b9eec5892112863c1c
ddlj123 <vp5966913@gmail.com>
2024-09-02 05:06:17 -0700

    episode-01

---
+++ app.js
@@ -0,0 +1,8 @@
+const heading=React.createElement("h1",{id:"heading"},"hello world from React");
+
+
+console.log(heading);
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
